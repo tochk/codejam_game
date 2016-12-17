2bc2c3d60191803851cb5011446c5844d30d90db
Alexey Andreev <bezumny.tochk@gmail.com>
2016-12-17 12:31:00 +0400

    Add sprites and map for tree

--- src/states/Tree.js
+++ src/states/Tree.js
@@ -5,20 +5,35 @@ import * as Phaser from "phaser";
 
 class Tree extends Phaser.State {
 
-    preload() {
+    constructor() {
+        super();
 
+        this.map = {};
+        this.layers = {};
     }
+
 
     create() {
         //buttons
         console.log("Loaded tree");
         this.stage.backgroundColor = "#4488AA";
+        this.createMap();
         this.toStartButton = this.game.add.text(20, 20, " ← Return to menu", {font: "30px Arial", fill: "#ffffff", stroke: "#000000", strokeThickness: 15});
         this.toStartButton.inputEnabled = true;
         this.toStartButton.input.useHandCursor = true;
         this.toStartButton.events.onInputDown.add(this.toStart, this);
         //end buttons block
+    }
 
+    createMap() {
+        this.map = this.game.add.tilemap('tree_map');
+        this.map.addTilesetImage('treeSprite');
+        this.layers.tree_back = this.map.createLayer('tree_base');
+        //TODO checking for level
+        this.layers.tree_third = this.map.createLayer('tree_third');
+        for (let key of Object.keys(this.layers)) {
+            this.layers[key].resizeWorld();
+        }
     }
 
     toStart() {
