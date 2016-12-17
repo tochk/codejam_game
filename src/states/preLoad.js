62872ef84e69ffb7b456cc5a0e6cda3dc6387728
Alexey Andreev <bezumny.tochk@gmail.com>
2016-12-17 16:21:56 +0400

    Fixed errors

--- src/states/preLoad.js
+++ src/states/preLoad.js
@@ -10,7 +10,7 @@ class Preload extends Phaser.State {
     preload() {
         this.game.load.tilemap('tilemap', 'data/map.json', null, Phaser.Tilemap.TILED_JSON);
         this.game.load.tilemap('map_level1', 'data/level1.json', null, Phaser.Tilemap.TILED_JSON);
-        this.game.load.tilemap('map_level2', 'data/map_level.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.tilemap('map_level2', 'data/level2.json', null, Phaser.Tilemap.TILED_JSON);
         this.game.load.image('coin', 'assets/coin.png', true);
         this.game.load.image('tiles', 'assets/tiles.png', true);
         this.game.load.image('snow_sprites', 'assets/snow_sprites.png', true);
