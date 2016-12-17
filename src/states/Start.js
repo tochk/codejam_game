923e22f4bc1deea134ef3e623bd7bc40a13ccbf4
Alexey Andreev <bezumny.tochk@gmail.com>
2016-12-17 16:15:58 +0400

    Added second level and achievements

--- src/states/Start.js
+++ src/states/Start.js
@@ -1,7 +1,15 @@
 /**
  * Created by tochk on 17.12.2016.
  */
-//import * as Phaser from "phaser";
+
+
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
 
 class Start extends Phaser.State {
 
@@ -15,15 +23,22 @@ class Start extends Phaser.State {
         let minTextColor = "#ffffff",
             textColor = "#52a6f9";
         this.title = this.createText(width/2, height/2 - 150, "-GAME-", 90, minTextColor);
-        this.toStartButton = this.createText(width/2, height/2 - 50, "START GAME!", 100, textColor);
-        this.openTree = this.createText(width/2, height/2 + 50, "Achievements tree", 80, minTextColor);
+        if (getCookie("level") != "3") {
+            this.toStartButton = this.createText(width / 2, height / 2 - 50, "START GAME!", 100, textColor);
+            this.toStartButton.inputEnabled = true;
+            this.toStartButton.input.useHandCursor = true;
+            this.toStartButton.events.onInputDown.add(this.startGame, this);
+        } else {
+            this.toStartButton = this.createText(width / 2, height / 2 - 50, "Game completed!", 80, textColor);
+        }
 
-        this.toStartButton.inputEnabled = true;
-        this.toStartButton.input.useHandCursor = true;
-        this.toStartButton.events.onInputDown.add(this.startGame, this);
+        this.openTree = this.createText(width/2, height/2 + 50, "Achievements tree", 80, minTextColor);
         this.openTree.inputEnabled = true;
         this.openTree.input.useHandCursor = true;
         this.openTree.events.onInputDown.add(this.showTree, this);
+        if (getCookie("data") != "ok") {
+            this.writeDataToCookie();
+        }
     }
 
     startGame() {
@@ -46,6 +61,16 @@ class Start extends Phaser.State {
         button.anchor.setTo(0.5, 0.5);
         button.align = 'center';
         return button;
+    }
+
+    writeDataToCookie() {
+        document.cookie = "data=ok";
+        document.cookie = "level=1";
+        document.cookie = "achievement1=1";
+        document.cookie = "achievement2=0";
+        document.cookie = "achievement3=0";
+        document.cookie = "achievement4=0";
+        document.cookie = "achievement5=0";
     }
 }
 
--- src/states/mainGame.js
+++ src/states/mainGame.js
@@ -2,26 +2,30 @@
  * Created by tochk on 17.12.2016.
  */
 
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
 class MainGame extends Phaser.State {
 
     constructor() {
         super();
-
         this.map = {};
         this.layers = {};
+        this.loadSavedData();
     }
 
 
     create() {
         console.log("Loaded maingame");
-
         this.game.stage.backgroundColor = '#addeff';
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.game.physics.arcade.gravity.y = 200;
-
         this.createMap();
         this.createPlayer();
-
         this.jumpTimer = 0;
         this.cursors =  this.game.input.keyboard.createCursorKeys();
         this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -30,9 +34,7 @@ class MainGame extends Phaser.State {
 
     update() {
         this.game.physics.arcade.collide(this.player, this.layers.map);
-        this.game.physics.arcade.collide(this.player, this.layers.end, this.end_game, null, this);
-        this.game.physics.arcade.overlap(this.player, this.coins2, this.collectCoin, null, this);
-
+        this.game.physics.arcade.collide(this.player, this.layers.end, this.winGame, null, this);
         this.player.body.velocity.x = 0;
         if (this.cursors.left.isDown)
         {
@@ -50,23 +52,68 @@ class MainGame extends Phaser.State {
         }
 
         if (this.player.position.y == 610) {
-            console.log(this.player.position.y);
-            this.end_game();
+            this.loseGame();
+        }
+
+        if (this.level == 2) {
+            this.game.physics.arcade.collide(this.player, this.layers.block, this.loseGame, null, this);
+        }
+    }
+
+    loadSavedData () {
+        if (getCookie("data") == "ok") {
+            console.log("Loaded saved data");
+            this.level = parseInt(getCookie("level"));
+            this.achievement1 = parseInt(getCookie("achievement1"));
+            this.achievement2 = parseInt(getCookie("achievement2"));
+            this.achievement3 = parseInt(getCookie("achievement3"));
+            this.achievement4 = parseInt(getCookie("achievement4"));
+            this.achievement5 = parseInt(getCookie("achievement5"));
         }
     }
 
     createMap() {
-        this.map = this.game.add.tilemap('map_level1');
-        this.map.addTilesetImage('coin');
-        this.map.addTilesetImage('tiles');
-        this.map.addTilesetImage('snow_sprites');
-        this.layers.map = this.map.createLayer('platforms');
-        this.layers.end = this.map.createLayer('end');
-        for (let key of Object.keys(this.layers)) {
-            this.layers[key].resizeWorld();
+        if (this.level == 1) {
+            this.map = this.game.add.tilemap('map_level1');
+            this.map.addTilesetImage('coin');
+            this.map.addTilesetImage('tiles');
+            this.map.addTilesetImage('snow_sprites');
+            this.layers.map = this.map.createLayer('platforms');
+            this.layers.end = this.map.createLayer('end');
+            for (let key of Object.keys(this.layers)) {
+                this.layers[key].resizeWorld();
+            }
+            this.map.setCollision([2, 3, 17, 18], true, this.layers.map);
+            this.map.setCollision([24, 29], true, this.layers.end);
+        } else if (this.level == 2) {
+            /*this.map = this.game.add.tilemap('map_level2');
+            this.map.addTilesetImage('tiles');
+            this.map.addTilesetImage('snow_sprites');
+            this.layers.map = this.map.createLayer('platforms');
+            this.layers.block = this.map.createLayer('block');
+            this.layers.end = this.map.createLayer('end');
+            this.layers.map.resizeWorld();
+            this.layers.block.resizeWorld();
+            this.layers.end.resizeWorld();
+            debugger;
+            for (let key of Object.keys(this.layers)) {
+                this.layers[key].resizeWorld();
+            }
+            this.map.setCollision([2, 3, 17, 18], true, this.layers.map);
+            this.map.setCollision([71, 77, 81, 8876,  75, 72, 76, 74], true, this.layers.block);
+            this.map.setCollision([24, 29], true, this.layers.end);*/
+            this.map = this.game.add.tilemap('map_level1');
+            this.map.addTilesetImage('coin');
+            this.map.addTilesetImage('tiles');
+            this.map.addTilesetImage('snow_sprites');
+            this.layers.map = this.map.createLayer('platforms');
+            this.layers.end = this.map.createLayer('end');
+            for (let key of Object.keys(this.layers)) {
+                this.layers[key].resizeWorld();
+            }
+            this.map.setCollision([2, 3, 17, 18], true, this.layers.map);
+            this.map.setCollision([24, 29], true, this.layers.end);
         }
-        this.map.setCollision([2, 3, 17, 18], true, this.layers.map);
-        this.map.setCollision([24, 29], true, this.layers.end);
     }
 
     createPlayer() {
@@ -77,9 +124,14 @@ class MainGame extends Phaser.State {
         this.game.physics.arcade.gravity.y = 300;
     }
 
-    end_game() {
+    loseGame() {
         this.game.state.start("GameOver");
     }
+
+    winGame() {
+        this.game.state.start("Success");
+    }
+
 }
 
 export default MainGame;
